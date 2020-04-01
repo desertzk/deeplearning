91f5bc42a8724d2c0c100df5ab4cf76b203a7264
zk <309410281@qq.com>
2020-04-01 22:55:00 +0800

    fix plt can't draw problem

--- week1/assignment3/planar_utils.py
+++ week1/assignment3/planar_utils.py
@@ -18,8 +18,8 @@ def plot_decision_boundary(model, X, y):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.ylabel('x2')
     plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
-    
+    plt.scatter(X[0, :], X[1, :], cmap=plt.cm.Spectral)
+
 
 def sigmoid(x):
     """
@@ -49,18 +49,18 @@ def load_planar_dataset():
         r = a*np.sin(4*t) + np.random.randn(N)*0.2 # radius
         X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
         Y[ix] = j
-        
+
     X = X.T
     Y = Y.T
 
     return X, Y
 
-def load_extra_datasets():  
+def load_extra_datasets():
     N = 200
     noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.5, noise=.3)
     noisy_moons = sklearn.datasets.make_moons(n_samples=N, noise=.2)
     blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
     gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(mean=None, cov=0.5, n_samples=N, n_features=2, n_classes=2, shuffle=True, random_state=None)
     no_structure = np.random.rand(N, 2), np.random.rand(N, 2)
-    
+
     return noisy_circles, noisy_moons, blobs, gaussian_quantiles, no_structure
